55735ca384c3b9b553361f1c13c67fd6d8ff3dc6
Naveen Goswami <naveen.goswami@springer.com>
2018-11-14 23:03:25 +0100

    fixed typo

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -76,9 +76,7 @@ func (bkr *BrokerImpl) Services(ctx context.Context) ([]brokerapi.Service, error
 			Name:                 bkr.Config.ServiceName,
 			Description:          "Shared service for sending logs to ES in GCP",
 			Bindable:             true,
-			Requires: []brokerapi.RequiredPermission{
-				brokerapi.RequiredPermission: "syslog_drain",
-				},
+			Requires[0]: brokerapi.PermissionSyslogDrain,
 			InstancesRetrievable: bkr.Config.FakeStateful,
 			BindingsRetrievable:  bkr.Config.FakeStateful,
 			Metadata: &brokerapi.ServiceMetadata{
